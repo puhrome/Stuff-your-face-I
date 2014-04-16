c0272bac443d95953b6714baf425735e698c3888
puhrome <phirom.yim@fullsail.edu>
2014-04-16 16:08:47 -0400

    Stuff Your Face
    Conditional expressions using if and else statements in WebStorm

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 /**
  * Stuff your face I Created by Phirom Yim on 4/14/14.
  */
-var weight = 250;
-var competitorWeight = 250;
+var weight = 249;
 //if competitor weighs enough they can qualify
 //if competitor doesn't weigh enough the need to gain some weight
-
-var qualification;
-qualification = (competitorWeight >= 250)? "The competitor qualifies for the heavy weight division" : "The competitor needs to gain some weight!";
-console.log(qualification);
+if( weight >= 250){
+    console.log("The competitor qualifies for the heavyweight division.");
+}else{
+    console.log("The competitor needs to gain some weight!");
+}
